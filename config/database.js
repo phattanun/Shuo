3188481d7534c18abda8bbe735382c739c0406df
Phattanun Aukkarapuntouch <Phattanun Aukkarapuntouch>
2016-04-20 23:25:34 +0700

    no message

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-'url' : 'mongodb://localhost:27017/Shuo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+'url' : 'mongodb://localhost:27017/Shuo'
 
 };
